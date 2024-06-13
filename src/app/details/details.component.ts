a0ad0edc5a99e70498a2fd707b2ac3fcb440837e
JesusMartinezClavel <jmcvalles@gmail.com>
2024-06-13 18:57:22 +0200

    FEAT -> Added content and style to details component

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -9,20 +9,36 @@ import { HousingLocation } from "../housing-location";
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      details works! {{housingLocation?.id}}
-    </p>
+  <article>
+    <img class="listing-photo" [src]="housingLocation?.photo">
+    <section class="listting-description">
+      <h2 class="listing-heading">{{housingLocation?.name}}</h2>
+      <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
+    </section>
+    <section class="listting-features">
+  <h2 class="section-heading">About this housing location</h2>
+  <ul>
+    <li>Units available: {{housingLocation?.availableUnits}}</li>
+    <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
+    <li>Does this locations have laundry: {{housingLocation?.laundry}}</li>
+  </ul>
+    </section>
+    <section class="listing-apply">
+      <h2 class="section-heading">Apply now to live here</h2>
+      <button class="primary" type="button">Apply now!</button>
+    </section>
+  </article>
   `,
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent {    
+export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute)
   housingService = inject(HousingService)
   housingLocation: HousingLocation | undefined
-  
+
   constructor() {
-    const housingLocationsId = Number(this.route.snapshot.params['id'])  
-    
+    const housingLocationsId = Number(this.route.snapshot.params['id'])
+
     this.housingLocation = this.housingService.getHousingLocationsById(housingLocationsId)
   }
 }
